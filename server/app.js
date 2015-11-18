21b251e0465a247d4580bcff3398da1cdf2b2bca
rob <rob@mitrend.com>
2015-11-18 15:58:10 -0500

    change port

--- server/app.js
+++ server/app.js
@@ -37,7 +37,7 @@
 
 	app.use( '/', routes );
 
-	app.set( 'port', process.env.PORT || 7564 );
+	app.set( 'port', process.env.PORT || 5353 );
 
 	var server = app.listen( app.get( 'port' ), function () {
 		console.log( 'Express server listening on port ' + server.address().port );
